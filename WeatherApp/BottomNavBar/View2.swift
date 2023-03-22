99098600a3a27db53706b2bee1aa3798412bb7be
Dawid Wydra <72543889+ejsiik@users.noreply.github.com>
2023-03-22 20:02:20 +0100

    working list
    multiple wrong city name

--- WeatherApp/BottomNavBar/View2.swift
+++ WeatherApp/BottomNavBar/View2.swift
@@ -18,20 +18,7 @@ struct View2: View {
                     addLocation()
                 }
             }.padding()
-            NavigationView {
-                List {
-                    ForEach(locations) { location in
-                        Text(location.name)
-                            .onTapGesture {
-                                print(location.id)
-                            }
-                    }
-                    .onDelete(perform: delete)
-                }
-                .navigationTitle("Locations")
-                .navigationBarItems(trailing: EditButton())
-            }
-            /*List {
+            List {
                 ForEach(locations) { location in
                     Text(location.name)
                         .onTapGesture {
@@ -39,7 +26,7 @@ struct View2: View {
                         }
                 }
                 .onDelete(perform: delete)
-            }*/
+            }
         }
         .onAppear {
             loadData()
@@ -47,7 +34,7 @@ struct View2: View {
     }
     
     func loadData() {
-        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=f1713ff8f3edf7b7afd6a48d1bd6c659") {
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Gliwice&appid=f1713ff8f3edf7b7afd6a48d1bd6c659") {
             URLSession.shared.dataTask(with: url) { data, response, error in
                 if let data = data {
                     do {
@@ -63,6 +50,35 @@ struct View2: View {
             }.resume()
         }
     }
+    /*func loadData() {
+        for location in locations {
+            if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(location.latitude)&lon=\(location.longitude)&appid=f1713ff8f3edf7b7afd6a48d1bd6c659") {
+                URLSession.shared.dataTask(with: url) { data, response, error in
+                    if let data = data {
+                        do {
+                            let decoder = JSONDecoder()
+                            let result = try decoder.decode(WeatherResult.self, from: data)
+                            DispatchQueue.main.async {
+                                print("Weather for \(location.name): \(result.weather.first?.description ?? "unknown")")
+                            }
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }.resume()
+            }
+        }
+    }*/
+    
+    struct WeatherResult: Codable {
+        let weather: [Weather]
+    }
+
+    struct Weather: Codable {
+        let description: String
+    }
+    
+    
     func addLocation() {
         if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=f1713ff8f3edf7b7afd6a48d1bd6c659") {
             URLSession.shared.dataTask(with: url) { data, response, error in
@@ -82,6 +98,47 @@ struct View2: View {
             }.resume()
         }
     }
+    
+    /*func addLocation() {
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=f1713ff8f3edf7b7afd6a48d1bd6c659") {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(Location.self, from: data)
+                        DispatchQueue.main.async {
+                            if !locations.contains(where: { $0.id == result.id }) {
+                                locations.append(result)
+                                self.fetchWeather(for: result)
+                            }
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
+    }*/
+
+    /*func fetchWeather(for location: Location) {
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(location.latitude)&lon=\(location.longitude)&appid=f1713ff8f3edf7b7afd6a48d1bd6c659") {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(WeatherResult.self, from: data)
+                        DispatchQueue.main.async {
+                            print("Weather for \(location.name): \(result.weather.first?.description ?? "unknown")")
+                        }
+                    } catch {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
+    }*/
+
+    
     func delete(at offsets: IndexSet) {
             locations.remove(atOffsets: offsets)
         }
--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -11,50 +11,82 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            /*if let weather = weather {
+                TabView(selection: $selection) {
+                    View1()
+                        .tabItem{
+                            Image(systemName: "location")
+                        }.tag(0)
+                    WeatherView(weather: weather)
+                        .tabItem{
+                            Image(systemName: "sun.max")
+                        }.tag(1)
+                    View2()
+                        .tabItem{
+                            Image(systemName: "heart.fill")
+                        }.tag(2)
+                }.tabViewStyle(PageTabViewStyle())
+            }
+
+            TabView() {
+                WelcomeView()
+                    .tabItem{
+                        Image(systemName: "location")
+                    }.tag(0)
+                View1()
+                    .tabItem{
+                        Image(systemName: "sun.max")
+                    }.tag(1)
+                View2()
+                    .tabItem{
+                        Image(systemName: "heart.fill")
+                    }.tag(2)
+            }.tabViewStyle(PageTabViewStyle())*/
+
             if let location = locationManager.location {
-                if let weather = weather {
-                    TabView(selection: $selection) {
-                        View1()
-                            .tabItem{
-                                Image(systemName: "location")
-                            }.tag(0)
-                        WeatherView(weather: weather)
-                            .tabItem{
-                                Image(systemName: "sun.max")
-                            }.tag(1)
-                        View2()
-                            .tabItem{
-                                Image(systemName: "heart.fill")
-                            }.tag(2)
-                    }
-                    .tabViewStyle(PageTabViewStyle())
-                } else {
-                    LoadingView()
-                        .task {
-                            do {
-                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
-                            } catch {
-                                print("Error getting weather: \(error)")
-                            }
-                            do {
+             if let weather = weather {
+                 TabView(selection: $selection) {
+                     View1()
+                         .tabItem{
+                             Image(systemName: "location")
+                         }.tag(0)
+                     WeatherView(weather: weather)
+                         .tabItem{
+                             Image(systemName: "sun.max")
+                         }.tag(1)
+                     View2()
+                         .tabItem{
+                             Image(systemName: "heart.fill")
+                         }.tag(2)
+                 }
+             .tabViewStyle(PageTabViewStyle())
+             } else {
+                 LoadingView()
+                     .task {
+                         do {
                              weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
-                            } catch {
-                                print("Error getting weather: \(error)")
-                            }
-                            do {
-                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
-                            } catch {
-                                print("Error getting weather: \(error)")
-                                let url = Bundle.main.url(forResource: "weatherData", withExtension: "json")!
-                                do {
-                                    let data = try Data(contentsOf: url)
-                                    weather = try JSONDecoder().decode(ResponseBody.self, from: data)
-                                } catch {
-                                    print("Error parsing local weather data: \(error)")
-                                }
-                            }
-                        }
-                }
+                         } catch {
+                             print("Error getting weather: \(error)")
+                         }
+                         /*do {
+                          weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                          } catch {
+                          print("Error getting weather: \(error)")
+                          }*/
+                         /*do {
+                          weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                          } catch {
+                          print("Error getting weather: \(error)")
+                          let url = Bundle.main.url(forResource: "weatherData", withExtension: "json")!
+                          do {
+                          let data = try Data(contentsOf: url)
+                          weather = try JSONDecoder().decode(ResponseBody.self, from: data)
+                          } catch {
+                          print("Error parsing local weather data: \(error)")
+                          }
+                          }*/
+                     }
+             }
             } else {
                 if locationManager.isLoading {
                     LoadingView()
@@ -66,57 +98,57 @@ struct ContentView: View {
         }
         .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
         .preferredColorScheme(.dark)
-
-    /*var body: some View {
-        VStack {
-            if let location = locationManager.location {
-                TabView(selection: $selection) {
-                    View1()
-                        .tabItem{
-                            Image(systemName: "location")
-                        }.tag(0)
-                    if let weather = weather {
-                        WeatherView(weather: weather)
-                            .tabItem{
-                                Image(systemName: "sun.max")
-                            }.tag(1)
-                    } else {
-                        LoadingView()
-                            .task {
-                                do {
-                                    weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
-                                } catch {
-                                    print("Error getting weather: \(error)")
-                                    let url = Bundle.main.url(forResource: "weatherData", withExtension: "json")!
-                                    do {
-                                        let data = try Data(contentsOf: url)
-                                        weather = try JSONDecoder().decode(ResponseBody.self, from: data)
-                                    } catch {
-                                        print("Error parsing local weather data: \(error)")
-                                    }
-                                }
-                            }
-                    }
-                    View2(items: ["Item 1", "Item 2", "Item 3"])
-                        .tabItem{
-                            Image(systemName: "heart.fill")
-                        }.tag(2)
-                }
-                .tabViewStyle(PageTabViewStyle())
-            } else {
-                if locationManager.isLoading {
-                    LoadingView()
-                } else {
-                    WelcomeView()
-                        .environmentObject(locationManager)
-                }
-            }
-        }
-        .onAppear {
-            if locationManager.status == .notDetermined {
-                $locationManager.requestAuthorization
-            }
-        }*/
+        
+        /*var body: some View {
+         VStack {
+         if let location = locationManager.location {
+         TabView(selection: $selection) {
+         View1()
+         .tabItem{
+         Image(systemName: "location")
+         }.tag(0)
+         if let weather = weather {
+         WeatherView(weather: weather)
+         .tabItem{
+         Image(systemName: "sun.max")
+         }.tag(1)
+         } else {
+         LoadingView()
+         .task {
+         do {
+         weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+         } catch {
+         print("Error getting weather: \(error)")
+         let url = Bundle.main.url(forResource: "weatherData", withExtension: "json")!
+         do {
+         let data = try Data(contentsOf: url)
+         weather = try JSONDecoder().decode(ResponseBody.self, from: data)
+         } catch {
+         print("Error parsing local weather data: \(error)")
+         }
+         }
+         }
+         }
+         View2(items: ["Item 1", "Item 2", "Item 3"])
+         .tabItem{
+         Image(systemName: "heart.fill")
+         }.tag(2)
+         }
+         .tabViewStyle(PageTabViewStyle())
+         } else {
+         if locationManager.isLoading {
+         LoadingView()
+         } else {
+         WelcomeView()
+         .environmentObject(locationManager)
+         }
+         }
+         }
+         .onAppear {
+         if locationManager.status == .notDetermined {
+         $locationManager.requestAuthorization
+         }
+         }*/
     }
 }
 
