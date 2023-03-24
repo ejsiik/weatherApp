278bf265a7b9c452a1ed1f92855614bfc57f60c2
Dawid Wydra <72543889+ejsiik@users.noreply.github.com>
2023-03-24 22:50:30 +0100

    obsługa błędów

--- WeatherApp/Managers/FavouriteLocationManager.swift
+++ WeatherApp/Managers/FavouriteLocationManager.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-
 class FavouriteLocationManager: ObservableObject {
     @Published var locations = [Location]()
+    @Published var updated = false
     
     init() {
         loadLocations()
@@ -15,17 +15,16 @@ class FavouriteLocationManager: ObservableObject {
         let locations = try? JSONDecoder().decode([Location].self, from: locationsData)
         self.locations = locations ?? []
     }
-
+    
     func addLocation(_ locationName: String) {
         guard !locationName.isEmpty else { return }
-        if locations.contains(where: { $0.name.lowercased() == locationName.lowercased() }) {
+        if locations.contains(where: { $0.name.lowercased() == locationName.removingPercentEncoding?.lowercased().replacingOccurrences(of: "+", with: " ")}) {
             // Location is already in the list
             return
         }
         fetchWeatherData(forCity: locationName)
     }
-
-    // for what? xD
+    
     private func fetchWeatherData(forCity city: String) {
         let apiKey = "f1713ff8f3edf7b7afd6a48d1bd6c659"
         let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
@@ -44,7 +43,7 @@ class FavouriteLocationManager: ObservableObject {
                 DispatchQueue.main.async {
                     let newLocation = Location(name: weatherData.name)
                     self.locations.append(newLocation)
-                    self.saveLocations()
+                    self.updateLocations()
                 }
             } catch {
                 print("Error decoding weather data: \(error.localizedDescription)")
@@ -56,6 +55,15 @@ class FavouriteLocationManager: ObservableObject {
     func saveLocations() {
         let locationsData = try? JSONEncoder().encode(locations)
         UserDefaults.standard.set(locationsData, forKey: "locations")
+    }
+    
+    private func updateLocations() {
+        let uniqueLocations = Dictionary(grouping: locations, by: { $0.name })
+            .values
+            .compactMap { $0.first }
+        self.locations = uniqueLocations
+        self.locations.sort { $0.name.localizedCaseInsensitiveCompare($1.name) == .orderedAscending }
+        self.saveLocations()
     }
 }
 
--- WeatherApp/Views/WelcomeView.swift
+++ WeatherApp/Views/WelcomeView.swift
@@ -1,18 +1,30 @@
 import SwiftUI
 import CoreLocationUI
+import UIKit
 
 struct WelcomeView: View {
     @EnvironmentObject var locationManager: LocationManager
     @State private var locationName = ""
-    
+    @State private var showAlert = false
+    @State private var alertMessage = ""
+
     func search(city: String) async {
+        locationManager.isLoading = true
         do {
-            try await locationManager.requestLocationByCity(city: locationName)
-        } catch {
-            print("Error")
+            if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene,
+               let window = windowScene.windows.first(where: { $0.isKeyWindow }),
+               let windowScene = window.windowScene {
+                try await locationManager.requestLocationByCity(city: city, presentingViewController: (windowScene.windows.first?.rootViewController)!)
+            }
+        } catch let error as NSError {
+            await MainActor.run {
+                alertMessage = error.localizedDescription
+                showAlert = true
+            }
         }
+        locationManager.isLoading = false
     }
-    
+
     var body: some View {
         VStack {
             VStack(spacing: 20) {
@@ -25,17 +37,17 @@ struct WelcomeView: View {
             }
             .multilineTextAlignment(.center)
             .padding()
-
+            
             
             // LocationButton from CoreLocationUI framework imported above, allows us to requestionLocation
             VStack{
                 LocationButton(.shareCurrentLocation) {
                     locationManager.requestLocation()
-            }
-            .cornerRadius(30)
-            .symbolVariant(.fill)
-            .foregroundColor(.white)
-            
+                }
+                .cornerRadius(30)
+                .symbolVariant(.fill)
+                .foregroundColor(.white)
+                
                 Text("or").padding()
                 
                 HStack {
@@ -43,13 +55,22 @@ struct WelcomeView: View {
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .fixedSize()
                     Button("Search") {
-                        Task { await search(city: locationName) }
+                        let replaced = (locationName as NSString).replacingOccurrences(of: " ", with: "+")
+                        let correct = replaced.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+                        if !correct.isEmpty {
+                            Task { await search(city: correct)
+                                locationName = ""
+                            }
+                        }
                     }
                 }
-
+                
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+        }
     }
 }
 
